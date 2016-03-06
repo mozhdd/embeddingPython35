6ce60834a937db7303118305f94f80f114f756aa
mozhdd <varzegov_evgenii@mail.ru>
2016-03-07 01:36:26 +0300

    with matrix (before cleaning)

---
+++ embeddingPython35/embeddingPython35/IncPath.h
@@ -0,0 +1,14 @@
+#pragma once
+//includeing array object from numpy
+#include "C:\Python351-32\Lib\site-packages\numpy\core\include\numpy\arrayobject.h"
+
+//initialize path of our embedded script
+//char* modPath = "D:/Python_proj/";
+char* modPath = "D:\\Python_proj\\Las/";
+
+//initialize name of our script and the name of function
+//char* modName = "myMod";
+//char* funcName = "SummArr";
+
+char* modName = "carrot";
+char* funcName = "WeveTransform";
--- embeddingPython35/embeddingPython35/main.cpp
+++ embeddingPython35/embeddingPython35/main.cpp
@@ -12,30 +12,30 @@
 #endif
 #include <iostream>
 using namespace std;
-#include "C:\Python351-32\Lib\site-packages\numpy\core\include\numpy\arrayobject.h"
-
+#include "IncPath.h"
+//#include "C:\Python351-32\Lib\site-packages\numpy\core\include\numpy\arrayobject.h"
+#include <fstream>
 
 class Signal
 {
 private:
-	int len;
-	double* net;
+	int len, len2;
+	double* lattice;
 	double* signal;
 
 public:
 	Signal();
-	Signal(int ln, double* nt, double* sig) : len(ln), net(nt), signal(sig) {}
+	Signal(int ln, int ln2, double* lt, double* sig) : len(ln), len2(ln2), lattice(lt), signal(sig) {}
 
 	int getLen() {
 		return len;
 	}
 	double* getNet() {
-		return net;
+		return lattice;
 	}
 	double* getSig() {
 		return signal;
 	}
-
 
 	PyObject *makearray(double* arr, size_t size)
 	{
@@ -44,7 +44,7 @@ public:
 	}
 
 	// inserting custom dir to sys path
-	void addSysPath(char* ModPath)
+	void addSysPath()
 	{
 		PyObject *sys_path, *path;
 		sys_path = PySys_GetObject("path");
@@ -53,21 +53,21 @@ public:
 			cout << "sys path is not defined";
 			return;
 		}
-		//FIXME
 		//path = PyUnicode_FromString("D:/Python_proj/");
-		path = PyUnicode_FromString(ModPath);
+		path = PyUnicode_FromString(modPath);
 		PyList_Insert(sys_path, 0, path);		//inserting path to sys path
 		Py_DECREF(path);
 	}
-	double* compute(char *modName, char *funcName, char* ModPath)
+
+	double* compute()
 	{
 		PyObject *pModule, *pFunc, *pArgs, *pValue, *pArgTuple;
 		double* dArrOut = NULL;
 		PyArrayObject* np_ret;
 
 		Py_Initialize();
-		addSysPath(ModPath);		// Let's add our directory to a system path
-		import_array();		//initializing NumPy Arrays
+		addSysPath();		// Let's add our directory to a system path
+		import_array();				//initializing NumPy Arrays
 
 		if ((pModule = PyImport_ImportModule(modName)) != NULL)
 		{
@@ -76,7 +76,7 @@ public:
 			{
 				pArgTuple = PyTuple_New(2);	// creating argument to extend
 				npy_int dim = len;
-				PyTuple_SetItem(pArgTuple, 0, PyArray_SimpleNewFromData(1, &dim, NPY_DOUBLE, (void *)net));
+				PyTuple_SetItem(pArgTuple, 0, PyArray_SimpleNewFromData(1, &dim, NPY_DOUBLE, (void *)lattice));
 				PyTuple_SetItem(pArgTuple, 1, PyArray_SimpleNewFromData(1, &dim, NPY_DOUBLE, (void *)signal));
 
 				pValue = PyObject_CallObject(pFunc, pArgTuple);			// calling function
@@ -84,7 +84,8 @@ public:
 				if (pValue != NULL)
 				{
 					np_ret = reinterpret_cast<PyArrayObject*>(pValue);
-					int len{ PyArray_SHAPE(np_ret)[0] };
+					int leng{ PyArray_SHAPE(np_ret)[0] };
+					len = leng;
 					dArrOut = reinterpret_cast<double*>(PyArray_DATA(np_ret));
 
 					Py_DECREF(pValue);
@@ -97,34 +98,45 @@ public:
 		return dArrOut;
 	}
 
-	double compute2(char *modName, char *funcName)
-	{
-		PyObject *pModule, *pFunc, *pArgs, *pValue, *pArgTuple;
-		double np_ret;
-		Py_Initialize();
+	//double* computeMatr(char *modName, char *funcName)
+	//{
+	//	PyObject *pModule, *pFunc, *pArgs, *pValue, *pArgTuple;
+	//	double* dArrOut = NULL;
+	//	PyArrayObject* np_ret;
 
-		if ((pModule = PyImport_ImportModule(modName)) != NULL)
-		{
-			pFunc = PyObject_GetAttrString(pModule, funcName);		// get the function funcName from module
-			if (pFunc && PyCallable_Check(pFunc))
-			{
-				pArgTuple = PyTuple_New(1);	// creating argument to extend
-				PyTuple_SetItem(pArgTuple, 0, PyLong_FromLong(0));
+	//	Py_Initialize();
+	//	addSysPath();		// Let's add our directory to a system path
+	//	import_array();				//initializing NumPy Arrays
 
-				pValue = PyObject_CallObject(pFunc, pArgTuple);			// calling function
-				Py_DECREF(pArgTuple);
-				if (pValue != NULL)
-				{
-					np_ret = PyFloat_AsDouble(pValue);
-					Py_DECREF(pValue);
-				}
-			}
-			Py_XDECREF(pFunc);										// can take NULL values as a parameters (unlike Py_DECREF)
-			Py_DECREF(pModule);
-		}
-		Py_Finalize();
-		return np_ret;
-	}
+	//	if ((pModule = PyImport_ImportModule(modName)) != NULL)
+	//	{
+	//		pFunc = PyObject_GetAttrString(pModule, funcName);		// get the function funcName from module
+	//		if (pFunc && PyCallable_Check(pFunc))
+	//		{
+
+	//			pArgTuple = PyTuple_New(1);	// creating argument to extend
+	//			npy_int dim = len;
+	//			npy_intp dims[2]{len, len2};
+	//			//PyTuple_SetItem(pArgTuple, 0, PyArray_SimpleNewFromData(2, dims, NPY_DOUBLE, (void *)signal));
+	//			const int ND{ 2 };
+	//			PyArrayObject *np_arg = reinterpret_cast<PyArrayObject*>(PyArray_SimpleNewFromData(ND, dims, NPY_LONGDOUBLE,
+	//				reinterpret_cast<void*>(signal)));
+	//			PyTuple_SetItem(pArgTuple, 0, reinterpret_cast<PyObject*>(np_arg));
+
+
+
+
+	//			PyObject_CallObject(pFunc, pArgTuple);			// calling function
+	//			Py_DECREF(pArgTuple);
+
+	//		}
+	//		Py_XDECREF(pFunc);										// can take NULL values as a parameters (unlike Py_DECREF)
+	//		Py_DECREF(pModule);
+	//	}
+	//	Py_Finalize();
+	//	return dArrOut;
+	//}
+
 
 };
 
@@ -132,22 +144,56 @@ public:
 void print(double* arr, int len)
 {
 	for (int i = 0; i < len; i++)
-		cout << arr[i] << " ";
+		cout << arr[i] << "\n";
 	cout << endl;
+}
+double* readArr(string path)
+{
+	int Len = 709;
+	double* arr = new double[Len];
+	ifstream file(path);
+
+	if (file.is_open())
+	{
+		double tmp;
+		for (int i = 0; i < Len; ++i)
+		{
+			file >> tmp;
+			arr[i] = tmp;
+		}
+	}
+	return arr;
 }
 
 int main(int argc, char *argv[])
 {
-	int len = 5;
-	Signal sig(len, new double[len] {3, 4, 5, 4, 3}, new double[len] {6, 5, 4, 5, 6});
+	int len = 3, len2 = 3;
+	//Signal sig(len, new double[len] {3, 4, 5, 4, 3}, new double[len] {6, 5, 4, 5, 6});
 
-	double* res = sig.compute("myMod", "SummArr", "D:/Python_proj/");
-	print(res, len);
+	//double* res = sig.compute("myMod", "SummArr");
+	//print(res, len);
 
-	/*Signal sig(5, new double[5] {3, 4, 5, 4, 3}, new double[5] {6, 5, 4, 5, 6});
-	double res = sig.compute2("math", "exp");
-	cout << res;*/
+	
+	double **x = new double* [len];
+	/*x[0] = new double[4]{13.75, NAN, NAN, 65.4392 };
+	x[1] = new double[4]{ 13.8, NAN, NAN, 65.4392 };
+	x[2] = new double[4]{ 719.0, NAN, NAN, -15.0};*/
 
+	x[0] = new double[len2]{ 13.75, 1, 65.4392 };
+	x[1] = new double[len2]{ 13.8, 2, 65.4392 };
+	x[2] = new double[len2]{ 719.0, 2, -15.0 };
+
+
+	len = 709;
+	Signal sig(len, len, readArr("D:\\Python_proj\\Las\\dept.txt"), readArr("D:\\Python_proj\\Las\\GR1.txt"));
+	double* res = sig.compute();
+	//print(res, sig.getLen());
+
+
+
+	//Signal sig(len, len2,  NULL, x);
+	//sig.computeMatr("myMod", "myPrint");
+	
 	getchar();
 	return 0;
 	//test settings
